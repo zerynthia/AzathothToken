87b516f346d6699044d0d44f4d742e44b08c0b68
vokaEntropy <60666903+vokaEntropy@users.noreply.github.com>
2023-06-21 21:11:08 +0300

    update IAzathothERC20.sol

--- contracts/IAzathothERC20.sol
+++ contracts/IAzathothERC20.sol
@@ -2,30 +2,24 @@
 
 pragma solidity ^0.8.0;
 
+// Standard
+// https://ethereum.org/en/developers/docs/standards/tokens/erc-20/
+// https://eips.ethereum.org/EIPS/eip-20
+
 interface IAzathothERC20 {
     // Functions
+    function name() external  view returns (string memory); // Optional
+    function symbol() external view returns (string memory); // Optional
+    function decimals() external view returns (uint8);  // Optional
 
-    function name() external view returns(string memory);
-
-    function symbol() external view returns(string memory);
-
-    function decimals() external pure returns(uint); // 0
-
-    function totalSupply() external view returns(uint);
-
-    function balanceOf(address account) external view returns(uint);
-
-    function transfer(address to, uint amount) external;
-
-    function allowance(address _owner, address spender) external view returns(uint);
-
-    function approve(address spender, uint amount) external;
-
-    function transferFrom(address sender, address recipient, uint amount) external;
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    function approve(address _spender, uint256 _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
 
     //Events
-
     event Transfer(address indexed from, address indexed to, uint amount);
-
     event Approve(address indexed owner, address indexed to, uint amount);
 }
