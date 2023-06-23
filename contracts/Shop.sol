66adae8d92a6849d8bbe0b5c334bd774a63c9a74
vokaEntropy <60666903+vokaEntropy@users.noreply.github.com>
2023-06-23 21:59:57 +0300

    feat: update shop.sol

--- contracts/Shop.sol
+++ contracts/Shop.sol
@@ -14,5 +14,44 @@ contract Shop {
 
     constructor() {
         token = new AzathothToken(address(this));
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not an owner!");
+        _;
+    }
+
+    function sell(uint _amountToSell) external {
+        require(
+            _amountToSell > 0 &&
+            token.balanceOf(msg.sender) >= _amountToSell,
+            "incorrect amount!"
+        );
+
+        uint allowance = token.allowance(msg.sender, address(this));
+        require(allowance >= _amountToSell, "check allowance!");
+
+        token.transferFrom(msg.sender, address(this), _amountToSell);
+
+        payable(msg.sender).transfer(_amountToSell);
+
+        emit Sold(_amountToSell, msg.sender);
+    }
+
+    function tokenBalance() public view returns(uint) {
+        return token.balanceOf(address(this));
+    }
+
+    receive() external payable {
+        uint tokensToBuy = msg.value; // 1 wei = 1 token
+        require(tokensToBuy > 0, "not enough funds!");
+
+        uint currentBalance = tokenBalance();
+        require(currentBalance >= tokensToBuy, "not enough tokens!");
+
+        token.transfer(msg.sender, tokensToBuy);
+
+        emit Bought(tokensToBuy, msg.sender);
     }
 }
